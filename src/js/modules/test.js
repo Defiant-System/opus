8e77baf9e28a7eed715234cc5010a97492634324
Hakan Bilgin <hbi99@hotmail.com>
2024-04-18 23:54:37 +0200

    more ui + code structure

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -2,7 +2,10 @@
 let Test = {
 	init(APP, Spawn) {
 
-		Spawn.el.find("layout").addClass("show-blank-view");
+		// Spawn.el.find("layout").addClass("show-blank-view");
+		Spawn.el.find("layout").addClass("show-sidebar-format show-sidebar-thumbs");
 		
+		Spawn.el.find("sidebar.format").addClass("show-slides");
+
 	}
 };
