2643cebd5bd064b9009b9d9df42b1cf792bd3f45
Hakan Bilgin <hbi99@hotmail.com>
2024-04-25 23:31:47 +0200

    overview ui/ux

--- src/js/modules/overview.js
+++ src/js/modules/overview.js
@@ -46,6 +46,9 @@
 				Spawn.find("layout")[value ? "removeClass" : "addClass"]("show-overview");
 				// return "state"
 				return !value;
+			case "add-slide":
+				console.log(event);
+				break;
 			case "select-slide":
 				el = $(event.target).parents("?li").get(0);
 				if (!el.length) return;
@@ -53,7 +56,10 @@
 				event.el.find(".active").removeClass("active");
 				el.addClass("active");
 
-				let ul = el,
+				let siblings = el.parent().find("> li"),
+					ultY = +el.parent().cssProp("--tY"),
+					ultX = +el.parent().cssProp("--tX"),
+					ul = el,
 					ulIndex = 0;
 				while (!ul.parent().hasClass("container")) {
 					ul = ul.parents("ul");
@@ -68,11 +74,17 @@
 
 				if (ulIndex % 2 === 1) {
 					options.push("n");
-					options.push("s");	
+					options.push("s");
+					if (el.index() === -ultX && ulIndex > 1) options = [];
+					if (el.index() === 0) options.push("e");
+					if (el.index() === siblings.length-1) options.push("w");
 				}
 				if (ulIndex % 2 === 0) {
 					options.push("e");
 					options.push("w");
+					if (el.index() === -ultY) options = [];
+					if (el.index() === 0) options.push("n");
+					if (el.index() === siblings.length-1) options.push("s");
 				}
 
 				// normalize options
@@ -88,9 +100,11 @@
 			Drag = Self.drag;
 		switch (event.type) {
 			case "mousedown":
+				// exit if event didn't originate in "overview root"
+				if (!$(event.target).hasClass("overview")) return;
 				// stop default behaviour
 				event.preventDefault();
-
+				// clear add-tools
 				Self.els.toolAdd.removeAttr("data-options");
 
 				let doc = $(document),
