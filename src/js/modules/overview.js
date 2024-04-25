2f49d9f9d6a46f5d3c8eb50ced3b5212ba7def6b
Hakan Bilgin <hbi99@hotmail.com>
2024-04-25 18:36:03 +0200

    overview ui/ux

--- src/js/modules/overview.js
+++ src/js/modules/overview.js
@@ -15,14 +15,49 @@
 			el;
 		// console.log(event);
 		switch (event.type) {
+			// native events
+			case "mouseover":
+				el = $(event.target);
+				// if (el.hasClass("overview")) {
+				// 	console.log(event.target);
+				// 	return Self.els.toolAdd.removeAttr("data-option");
+				// }
+
+				let [a, b, c, d, x, y] = Self.els.container.css("transform").replace(/matrix\(|\)/g, "").split(",").map(i => +i),
+					offset = el.offset(),
+					top = offset.top + y,
+					left = offset.left + x,
+					options = [];
+				
+				options.push("n");
+				// options.push("s");
+				// options.push("e");
+				// options.push("w");
+
+				Self.els.toolAdd
+					.css({ top, left })
+					.data({ option: options.join("") });
+				break;
 			// system events
 			case "spawn.focus":
+				// fast references
+				Self.els = {
+					container: Spawn.find(`.container`),
+					overview: Spawn.find(`.overview`),
+					toolAdd: Spawn.find(`.tool-add`),
+				};
 				// bind event handler
-				Spawn.find(`.overview`).on("mousedown", Self.doPan);
+				Self.els.container.on("mouseover", "li:not(.stack)", Self.dispatch);
+				// Self.els.overview.on("mouseover", Self.dispatch);
+				Self.els.overview.on("mousedown", Self.doPan);
 				break;
 			case "spawn.blur":
 				// unbind event handler
-				Spawn.find(`.overview`).off("mousedown", Self.doPan);
+				Self.els.container.off("mouseover", "li:not(.stack)", Self.dispatch);
+				// Self.els.overview.off("mouseover", Self.dispatch);
+				Self.els.overview.off("mousedown", Self.doPan);
+				// reset references
+				Self.els = {};
 				break;
 			// custom events
 			case "toggle-overview":
@@ -38,6 +73,13 @@
 				Spawn.find("layout")[value ? "removeClass" : "addClass"]("show-overview");
 				// return "state"
 				return !value;
+			case "select-slide":
+				el = $(event.target).parents("?li").get(0);
+				if (!el.length) return;
+				// active indicator
+				event.el.find(".active").removeClass("active");
+				el.addClass("active");
+				break;
 		}
 	},
 	doPan(event) {
@@ -51,10 +93,10 @@
 
 				let doc = $(document),
 					el = $(event.target).parents("?.overview").find(".container"),
-					tr = el.css("transform").replace(/matrix\(|\)/g, "").split(",").map(i => +i),
+					[a, b, c, d, x, y] = el.css("transform").replace(/matrix\(|\)/g, "").split(",").map(i => +i),
 					click = {
-						x: event.clientX - tr[4],
-						y: event.clientY - tr[5],
+						x: event.clientX - x,
+						y: event.clientY - y,
 					};
 
 				// create drag object
@@ -64,9 +106,9 @@
 				Self.drag.doc.on("mousemove mouseup", Self.doPan);
 				break;
 			case "mousemove":
-				let x = event.clientX - Drag.click.x,
-					y = event.clientY - Drag.click.y,
-					transform = `translate(${x}px, ${y}px)`;
+				let tX = event.clientX - Drag.click.x,
+					tY = event.clientY - Drag.click.y,
+					transform = `translate(${tX}px, ${tY}px)`;
 				Drag.el.css({ transform });
 				break;
 			case "mouseup":
